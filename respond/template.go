8315377ebc75dfacd7c733124381e2c5944a1edd
Jille Timmermans <jille@quis.cx>
2020-03-14 14:13:19 +0100

    Don't force people to pass in a TemplateProducer
    While I still like the model, it's awkward with Go's type system. For example `func() *template.Template` didn't match respond.TemplateProducer

--- respond/template.go
+++ respond/template.go
@@ -10,18 +10,15 @@ import (
 	"github.com/Jille/convreq/internal"
 )
 
-// TemplateProducer is a function that returns a template (like) object.
-// This is a function to encourage you to implement hot reloading for development servers.
-// For production servers we recommend parsing the template at startup and then have a closure that returns that.
-type TemplateProducer func() interface {
+// Template is a template-like object, like text/template.Template.
+type Template interface {
 	Execute(wr io.Writer, data interface{}) error
 }
 
 // RenderTemplate creates a response that will render the given template.
-// See TemplateProducer.
-func RenderTemplate(tplp TemplateProducer, data interface{}) internal.HttpResponse {
+func RenderTemplate(tpl Template, data interface{}) internal.HttpResponse {
 	var buf bytes.Buffer
-	if err := tplp().Execute(&buf, data); err != nil {
+	if err := tpl.Execute(&buf, data); err != nil {
 		return InternalServerError(fmt.Errorf("failed to render template: %v", err))
 	}
 	return Reader(&buf)
